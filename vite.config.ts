526692e997ff259d24d82f2458d85a5d4cb3d942
albivenan <149981434+albivenan@users.noreply.github.com>
2024-01-12 09:00:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  define: {
+    glonal: {}
+  },
   plugins: [react()],
   assetsInclude: ["**/*.docx", "**/*.doc", "**/*.ppt", "**/*.pptx", "**/*.xlsx"],
   optimizeDeps: {
